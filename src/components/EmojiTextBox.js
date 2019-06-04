8acc148483e94c242e7ff98142d9a4ab3a329861
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-04 12:04:20 +0530

    :bug: multiline emojifying bug fixed

--- src/components/EmojiTextBox.js
+++ src/components/EmojiTextBox.js
@@ -2,24 +2,34 @@ import React, { useState } from 'react'
 import EmojiContext from '../EmojiContext'
 
 const EmojiTextBox = props => {
-  const [text, setText] = useState('Hello 🌐!')
+  const [text, setText] = useState('')
+
   const emojify = data => {
+    const replaceLine = line =>
+      line
+        .split(' ')
+        .map(word => data.emojis[word.toLowerCase()] || word)
+        .join(' ')
+
     const emojifiedText = text
-      .split(' ')
-      .map(word => data.emojis[word.toLowerCase()] || word)
-      .join(' ')
+      .split('\n')
+      .map(replaceLine)
+      .join('\n')
+
     setText(emojifiedText)
   }
+
   return (
     <EmojiContext.Consumer>
       {data => (
         <textarea
           {...props}
           value={text}
+          placeholder="Start emojifying your text...."
           rows="10"
           cols="50"
           onKeyUp={({ keyCode }) => {
-            if (keyCode === 32) {
+            if (keyCode === 32 || keyCode === 13) {
               emojify(data)
             }
           }}
