f9c4f73a81dfb535ed20acb72a70936f40a07561
Akash Rajpurohit <akash009.dev@gmail.com>
2019-06-04 11:05:46 +0530

    :boom: Context provider working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './App.css'
 import EmojiTextBox from './components/EmojiTextBox'
+import EmojiContext from './EmojiContext'
 
 function App() {
   return (
     <div className="App">
       <h1>Emojify</h1>
-      <EmojiTextBox />
+      <EmojiContext.Provider
+        value={{
+          emojis: {
+            smile: '🔥',
+          },
+        }}
+      >
+        <EmojiTextBox />
+      </EmojiContext.Provider>
     </div>
   )
 }
